c7348f61d58d4e5d4bcb79f707dd149c2db66749
Jhonatan Gamboa <jhntngmb@gmail.com>
2023-06-14 16:55:24 -0500

    first commit

---
+++ app/Http/Controllers/TicketController.php
@@ -0,0 +1,83 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\CreateTicketRequest;
+use App\Http\Requests\UpdateTicketRequest;
+use App\Http\Resources\TicketResource;
+use App\Models\Ticket;
+
+class TicketController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $tickets = Ticket::paginate(10);
+        return TicketResource::collection($tickets);
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(CreateTicketRequest $request)
+    {
+        $ticket = Ticket::create([
+            'descripcion' => $request->input('descripcion'),
+            'usuario' => $request->input('usuario'),
+            'estado' => true,
+        ]);
+        $ticket->save();
+
+        return new TicketResource($ticket);
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        $ticket = Ticket::findOrFail($id);
+
+        return new TicketResource($ticket);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(UpdateTicketRequest $request, $id)
+    {
+        $ticket = Ticket::findOrFail($id);
+        $ticket->fill($request->all());
+        $ticket->save();
+
+        return new TicketResource($ticket);
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        $ticket = Ticket::findOrFail($id);
+        $ticket->delete();
+
+        return response()->json(['message' => 'Ticket eliminado']);
+    }
+}
---
+++ app/Models/Ticket.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class Ticket extends Model
+{
+    use HasFactory, SoftDeletes;
+
+    protected $fillable = ['descripcion', 'usuario', 'estado'];
+    protected $hidden = ['deleted_at'];
+}
